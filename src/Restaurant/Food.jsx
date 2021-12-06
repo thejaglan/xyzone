0459b01b303a439f6cb1d4940a48465507dc24f6
thejaglan <himanshujaglan1477@gmail.com>
2021-12-06 19:07:07 +0530

    first commit

---
+++ src/Restaurant/Food.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import "./style.css";
+import Menu from "./MenuApi.js";
+import MenuCard from './MenuCard';
+import Navbar from './Navbar';
+
+const uniqueList = [
+    ...new Set(
+        Menu.map((curElem) => {
+            return curElem.category;
+        })
+    ),
+    "All",
+];
+
+console.log(uniqueList);
+
+const Food = () => {
+    const [menuData, setMenuData] = useState(Menu);
+    const [menuList, setmenuList] = useState(uniqueList);
+
+    const filterItem = (category) => {
+        const updatedList = Menu.filter((curElem) => {
+            return curElem.category === category;
+        });
+        setMenuData(updatedList);
+    };
+
+
+    return (
+        <div>
+
+            <Navbar filterItem={filterItem} menuList={menuList}/>
+
+            <MenuCard menuData={menuData}/>
+        </div>
+    );
+};
+export default Food;
---
+++ src/Restaurant/MenuApi.js
@@ -0,0 +1,218 @@
+const Menu = [
+{
+    id: 1,
+    images: "image/coffee.jpg",
+    name: "Coffee",
+    category: "breakfast",
+    price: "75",
+    description: "I love Coffee loremafdsfdsdfsfdsfdsdfsfdsfdsdfs"
+},
+
+{
+    id: 2,
+    images: "image/cookies.jpg",
+    name: "Cookie",
+    category: "breakfast",
+    price: "75",
+    description: "I love Coffee loremafdsfdsdfsfdsfdsdfsfdsfdsdfs"
+},
+
+{
+    id: 3,
+    images: "image/omlet.jpg",
+    name: "Omlet",
+    category: "breakfast",
+    price: "75",
+    description: "I love Coffee loremafdsfdsdfsfdsfdsdfsfdsfdsdfs"
+},
+
+{
+    id: 4,
+    images: "image/fruit.jpg",
+    name: "Fruit",
+    category: "breakfast",
+    price: "75",
+    description: "I love Coffee loremafdsfdsdfsfdsfdsdfsfdsfdsdfs"
+},
+
+{
+    id: 5,
+    images: "image/greentea.jpg",
+    name: "Green Tea",
+    category: "breakfast",
+    price: "75",
+    description: "I love Coffee loremafdsfdsdfsfdsfdsdfsfdsfdsdfs"
+},
+
+{
+    id: 6,
+    images: "image/tea.jpg",
+    name: "Tea",
+    category: "breakfast",
+    price: "75",
+    description: "I love Coffee loremafdsfdsdfsfdsfdsdfsfdsfdsdfs"
+},
+
+{
+    id: 7,
+    images: "image/friedmeal.jpg",
+    name: "Fried Meal",
+    category: "lunch",
+    price: "75",
+    description: "I love Coffee loremafdsfdsdfsfdsfdsdfsfdsfdsdfs"
+},
+
+{
+    id: 8,
+    images: "image/whitecramic.jpg",
+    name: "White Cramic",
+    category: "lunch",
+    price: "75",
+    description: "I love Coffee loremafdsfdsdfsfdsfdsdfsfdsfdsdfs"
+},
+
+{
+    id: 9,
+    images: "image/kaddhi.jpg",
+    name: "Kaddhi",
+    category: "lunch",
+    price: "75",
+    description: "I love Coffee loremafdsfdsdfsfdsfdsdfsfdsfdsdfs"
+},
+
+{
+    id: 10,
+    images: "image/ricemeal.jpg",
+    name: "Rice-Meal",
+    category: "lunch",
+    price: "75",
+    description: "I love Coffee loremafdsfdsdfsfdsfdsdfsfdsfdsdfs"
+},
+
+{
+    id: 11,
+    images: "image/malaikofta.jpg",
+    name: "Malai Kofta",
+    category: "lunch",
+    price: "75",
+    description: "I love Coffee loremafdsfdsdfsfdsfdsdfsfdsfdsdfs"
+},
+
+{
+    id: 12,
+    images: "image/kadhaipaneer.jpg",
+    name: "Paneer Pyaji",
+    category: "lunch",
+    price: "75",
+    description: "I love Coffee loremafdsfdsdfsfdsfdsdfsfdsfdsdfs"
+},
+
+{
+    id: 13,
+    images: "image/sandwich.jpg",
+    name: "Sandwich",
+    category: "evening",
+    price: "75",
+    description: "I love Coffee loremafdsfdsdfsfdsfdsdfsfdsfdsdfs"
+},
+
+{
+    id: 14,
+    images: "image/pizza.jpg",
+    name: "Pizza",
+    category: "evening",
+    price: "155",
+    description: "I love Pizza loremafdsfdsdfsfdsfdsdfsfdsdfsfgddfsfgd"
+},
+{
+    id: 15,
+    images: "image/fruits.jpg",
+    name: "Fruits",
+    category: "evening",
+    price: "75",
+    description: "I love Coffee loremafdsfdsdfsfdsfdsdfsfdsfdsdfs"
+},
+
+{
+    id: 16,
+    images: "image/dosa.jpg",
+    name: "Dosa",
+    category: "evening",
+    price: "75",
+    description: "I love Shake loremafdsfdsdfsfdsfdsdfsfdsfdsdfs"
+},
+
+{
+    id: 17,
+    images: "image/chaatmasala.jpg",
+    name: "Chaat Masala",
+    category: "evening",
+    price: "75",
+    description: "I love Shake loremafdsfdsdfsfdsfdsdfsfdsfdsdfs"
+},
+
+{
+    id: 18,
+    images: "image/burger.jpg",
+    name: "Burger",
+    category: "evening",
+    price: "10",
+    description: "I love Burger Lassi loremafdsfdsdffdsfdsdfsfdsfd"
+},
+
+{
+    id: 19,
+    images: "image/dinner1.jpg",
+    name: "D-Meal",
+    category: "dinner",
+    price: "10",
+    description: "I love Burger Lassi loremafdsfdsdffdsfdsdfsfdsfd"
+},
+
+{
+    id: 20,
+    images: "image/f-paneer.jpg",
+    name: "Fried Paneer",
+    category: "dinner",
+    price: "10",
+    description: "I love Burger Lassi loremafdsfdsdffdsfdsdfsfdsfd"
+},
+
+{
+    id: 21,
+    images: "image/dinner3.jpg",
+    name: "Egg Rice",
+    category: "dinner",
+    price: "10",
+    description: "I love Burger Lassi loremafdsfdsdffdsfdsdfsfdsfd"
+},
+
+{
+    id: 22,
+    images: "image/roti.jpg",
+    name: "Roti",
+    category: "dinner",
+    price: "10",
+    description: "I love Burger Lassi loremafdsfdsdffdsfdsdfsfdsfd"
+},
+
+{
+    id: 23,
+    images: "image/mixveg.jpg",
+    name: "Mix-Veg",
+    category: "dinner",
+    price: "10",
+    description: "I love Burger Lassi loremafdsfdsdffdsfdsdfsfdsfd"
+},
+
+{
+    id: 24,
+    images: "image/dinner6.jpg",
+    name: "Fried Fish",
+    category: "dinner",
+    price: "10",
+    description: "I love Burger Lassi loremafdsfdsdffdsfdsdfsfdsfd"
+},
+
+];
+export default Menu;
